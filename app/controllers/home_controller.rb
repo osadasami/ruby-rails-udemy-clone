5ce232e6d36574f48316cc2a94083e3c69b4a491
osadasami <osadasami@gmail.com>
2022-03-29 17:30:33 +0500

    add some styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
   def index
+    @courses = Course.limit(5)
+    @latest_courses = Course.order(created_at: :desc).limit(3)
   end
 end
