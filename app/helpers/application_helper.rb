1f62aeac08b435d22f427d2bc42c10bb501ad82c
osadasami <osadasami@gmail.com>
2022-04-07 18:03:49 +0500

    add button to course to place a review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,16 @@ module ApplicationHelper
       return link_to "Buy for #{number_to_currency(course.price)}", new_enrollment_path(course: course)
     end
   end
+
+  def review_button(user, course)
+    return unless user && course.bought_by?(user)
+
+    user_enrollment = course.enrollments.find_by(user: user)
+
+    if user_enrollment.review.empty?
+      link_to 'Review', edit_enrollment_path(user_enrollment)
+    else
+      link_to 'Your review', enrollment_path(user_enrollment)
+    end
+  end
 end
